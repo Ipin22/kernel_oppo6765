a381e044a3b73b0502fa0a030fc7a826aedee0ab
Ipin 22 <ucihamala573@gmail.com>
2024-10-25 21:55:53 +0000

    add build script

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# set environment variables
+git clone --depth=1 https://github.com/Ipin22/clang clang
+export KBUILD_BUILD_HOST="los18.1"
+export KBUILD_BUILD_USER="ipin22"
+export PATH="$(pwd)/clang/bin:$PATH"
+export LOCALVERSION="-lineage"
+# build kernel
+make -j$(nproc --all) O=out ARCH=arm64 ipin_defconfig
+make -j16 ARCH=arm64 O=out \
+                      HOSTCC="ccache clang" \
+                      HOSTCXX="ccache clang++" \
+                      CC="ccache clang" \
+                      CROSS_COMPILE=aarch64-linux-gnu- \
+                      CROSS_COMPILE_ARM32=arm-linux-gnueabi-
